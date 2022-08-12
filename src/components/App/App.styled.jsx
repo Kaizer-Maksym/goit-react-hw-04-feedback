1abbc2fa4a5a3930df7eafebafc8207c97b7c3ff
Maksym Kaizer <kaizeriimaxim@gmail.com>
2022-08-12 16:27:33 +0200

    fix-02

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -7,13 +7,11 @@ export const ContextContainer = styled.div`
   margin: auto;
   padding: 20px 20px 20px 50px;
 
-  background-image: url('../../img/body-bg.png');
-
   border-radius: 10px;
   box-shadow: 8px 6px 3px rgba(0, 0, 0, 0.2), 8px 6px 3px rgba(0, 0, 0, 0.1),
     8px 6px 3px rgba(0, 0, 0, 0.3);
 
-  background-color: rgba(244, 244, 234, 0.7);
+  background-color: rgba(245, 245, 195, 0.8);
 
   font-family: 'Open sans';
   font-style: normal;
